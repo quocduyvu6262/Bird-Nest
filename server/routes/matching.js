4403f00c1b836c0c59b42cf4e3614aa59634ab0b
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-07-12 13:37:33 -0400

    Set up matching route

--- server/routes/matching.js
+++ server/routes/matching.js
@@ -5,3 +5,15 @@ const express = require('express');
 const db = require('../utils/database');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    const query = '';
+    db(query, (err, result) => {
+        if(err){
+            console.log(err);
+            res.status.send(`Bad request`);
+            return;
+        }
+        // Execute algorithm
+        
+    })
+})
