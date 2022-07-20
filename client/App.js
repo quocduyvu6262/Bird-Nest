54ce419bafede14a9ce358efab298e8d99afd364
Pinoyburn <deondre.garcia@yahoo.com>
2022-07-19 17:08:04 -0700

    auth

--- client/App.js
+++ client/App.js
@@ -3,9 +3,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 //Bottom Tab Navigation
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Tab = createBottomTabNavigator(); //Tab 
+const Tab = createBottomTabNavigator(); //Tab
 
 //Import screens in nav bar
 import BirdFeed from "./screens/BirdFeed.js";
@@ -13,11 +13,13 @@ import Profile from "./screens/Profile.js";
 import MessengerPigeon from "./screens/MessengerPigeon.js";
 import ChirpNotification from "./screens/ChirpNotification.js";
 import History from "./screens/History.js";
+import Login from "./screens/Login.js";
 
 const Stack = createNativeStackNavigator();
 
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import React, { useState } from "react";
+
 import {
   StyleSheet,
   Text,
@@ -27,48 +29,23 @@ import {
   TextInput,
   Button,
 } from "react-native";
-import image from "./assets/D85_6160.jpg";
 
 export default function App() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [userList, setUserList] = useState([]);
-
-  const addUser = () => {
-    Axios.post("http://localhost:5000/create", {
-      username: username,
-      email: email,
-      password: password,
-    })
-      .then(() => {
-        console.log("success");
-      })
-      .catch((error) => console.log(error));
-  };
-
-  const getUsers = () => {
-    Axios.get("http://localhost:5000/")
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => console.log(error));
-  };
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="WelcomeScreen"
+        initialRouteName="Login"
         screenOptions={{ headerShown: false }}
       >
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="BirdFeed" component={BirdFeed} />
         <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="MessengerPigeon" component={MessengerPigeon}/>
-        <Stack.Screen name="ChirpNotification" component={ChirpNotification}/>
-        <Stack.Screen name="History" component={History}/>
+        <Stack.Screen name="MessengerPigeon" component={MessengerPigeon} />
+        <Stack.Screen name="ChirpNotification" component={ChirpNotification} />
+        <Stack.Screen name="History" component={History} />
       </Stack.Navigator>
-
-      
     </NavigationContainer>
   );
 }
---
+++ client/screens/Login.js
@@ -0,0 +1,40 @@
+import { View, Text, SafeAreaView, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { AuthContext } from "../App";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+WebBrowser.maybeCompleteAuthSession();
+
+const Login = () => {
+  //   <AuthContext.Consumer>{console.log(value)}</AuthContext.Consumer>
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId:
+      "1037067763056-9s0pibe2utq867496i4necut03i2jam5.apps.googleusercontent.com",
+    // iosClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+    // androidClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+    // webClientId: "GOOGLE_GUID.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { authentication } = response;
+      console.log("Access Token print:" + authentication.accessToken);
+    }
+  }, [response]);
+
+  return (
+    <SafeAreaView>
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Login;
