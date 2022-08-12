c71dc4743b1e0fd33e61fa83d08ea11dfc0859d4
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-08-11 21:10:53 -0400

    fix matching

--- server/routes/matching.js
+++ server/routes/matching.js
@@ -6,7 +6,7 @@ const db = require('../utils/database');
 const router = express.Router();
 
 router.post('/', (req, res) => { // input
-	var provided_id = 9; //temporary until ID is provided by front-end
+	var provided_id = req.body.user_id; //temporary until ID is provided by front-end
 	//var provided_id = req.body.id:
 
 	//query for sending every user's variables to the front-end 
@@ -17,7 +17,7 @@ router.post('/', (req, res) => { // input
 			(err, result) => {
 				const provided_values = result;
 				//add the following matching variables to the map
-				must_have_map.set("neighborhood", provided_values[0].neighborhood);
+				//must_have_map.set("neighborhood", provided_values[0].neighborhood);
 				must_have_map.set("lease", provided_values[0].lease);
 				must_have_map.set("rent", provided_values[0].rent);
 				must_have_map.set("squarefeet", provided_values[0].squarefeet);
