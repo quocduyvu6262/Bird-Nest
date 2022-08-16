50bcaa3975894f7ba753d16a3b23a31688307e52
jmultani1 <jmultani@ucsd.edu>
2022-08-16 14:01:04 -0700

    Delete UI mostly done

--- client/screens/Profile.js
+++ client/screens/Profile.js
@@ -34,6 +34,11 @@ const Profile = ({ navigation }) => {
   let pics2 = ['file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/0A1630EF-0177-4E0C-8FBD-6BFB778F3263.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/14AEEF7D-3B83-4C8C-9A9C-298D9464E2CC.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/14B13BC3-149C-408F-99C1-0374078B9DB8.jpg']
   let pics3 = ['file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/D4824626-F148-479F-9067-0155BA03502B.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/BD98E0AD-0415-4BAC-A8BB-7545CBC17683.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/FFCF7FEF-FEE1-4BFD-B465-79B1A0F18ECC.jpg']
 
+ 
+  let count1 = 0;
+  let count2 = 0;
+  let count3 = 0;
+
   const data = useSelector(state => state.data);
   const [index, setIndex] = useState(0);
   const [name, setName] = useState();
@@ -43,7 +48,16 @@ const Profile = ({ navigation }) => {
   const [buttonClicked, setButtonClicked] = useState(false);
   const [interestButtonClicked, setInterestButtonClicked] = useState(false);
   const [deleteImage, setDeleteImage] = useState(false);
-  const [opacity, setOpacity] = useState(1);
+  const [opacity1, setOpacity1] = useState(1);
+  const [opacity2, setOpacity2] = useState(1);
+  const [opacity3, setOpacity3] = useState(1);
+  const [opacity4, setOpacity4] = useState(1);
+  const [opacity5, setOpacity5] = useState(1);
+  const [opacity6, setOpacity6] = useState(1);
+  const [opacity7, setOpacity7] = useState(1);
+  const [opacity8, setOpacity8] = useState(1);
+  const [opacity9, setOpacity9] = useState(1);
+  const [counter, setCounter] = useState(0);
 
   const roomInfoButton = () => {
     setButtonClicked(true);
@@ -58,59 +72,188 @@ const Profile = ({ navigation }) => {
   };
   const openDelete = () => {
     setDeleteImage(true);
-    console.log('ran true');
   }
   const closeDelete = () => {
     setDeleteImage(false);
-    console.log('ran false');
   }
   const changeIndex = () => {
     setIndex(index+1);
   }
-  const changeOpacity = () => {
-    setOpacity(0.5);
+  const changeOpacity1 = () => {
+    if(opacity1 == 1) {
+      setCounter(counter+1);
+      setOpacity1(0.5);
+    }
+    else if(opacity1 == 0.5) {
+      setCounter(counter-1);
+      setOpacity1(1);
+    }
+  }
+
+  const changeOpacity2 = () => {
+    if(opacity2 == 1) {
+      setCounter(counter+1);
+      setOpacity2(0.5);
+    }
+    else if(opacity2 == 0.5) {
+      setCounter(counter-1);
+      setOpacity2(1);
+    }
+  }
+
+  const changeOpacity3 = () => {
+    if(opacity3 == 1) {
+      setCounter(counter+1);
+      setOpacity3(0.5);
+    }
+    else if(opacity3 == 0.5) {
+      setCounter(counter-1);
+      setOpacity3(1);
+    }
+  }
+  const changeOpacity4 = () => {
+    console.log(count1);
+    if(opacity4 == 1) {
+      setCounter(counter+1);
+      setOpacity4(0.5);
+    }
+    else if(opacity4 == 0.5) {
+      setCounter(counter-1);
+      setOpacity4(1);
+    }
+  }
+
+  const changeOpacity5 = () => {
+    if(opacity5 == 1) {
+      setCounter(counter+1);
+      setOpacity5(0.5);
+    }
+    else if(opacity5 == 0.5) {
+      setCounter(counter-1);
+      setOpacity5(1);
+    }
+  }
+
+  const changeOpacity6 = () => {
+    if(opacity6 == 1) {
+      setCounter(counter+1);
+      setOpacity6(0.5);
+    }
+    else if(opacity6 == 0.5) {
+      setCounter(counter-1);
+      setOpacity6(1);
+    }
+  }
+  const changeOpacity7 = () => {
+    if(opacity7 == 1) {
+      setCounter(counter+1);
+      setOpacity7(0.5);
+    }
+    else if(opacity7 == 0.5) {
+      setCounter(counter-1);
+      setOpacity7(1);
+    }
+  }
+
+  const changeOpacity8 = () => {
+    if(opacity8 == 1) {
+      setCounter(counter+1);
+      setOpacity8(0.5);
+    }
+    else if(opacity8 == 0.5) {
+      setCounter(counter-1);
+      setOpacity8(1);
+    }
+  }
+
+  const changeOpacity9 = () => {
+    if(opacity9 == 1) {
+      setCounter(counter+1);
+      setOpacity9(0.5);
+    }
+    else if(opacity9 == 0.5) {
+      setCounter(counter-1);
+      setOpacity9(1);
+    }
   }
   // return screen
-  let count1 = 0;
   var images1 = pics1.map(function(image) {
     if(count1 == 0) {
-      count1 = count1 + 1;
+      count1 = 1;
       return (
-        <TouchableOpacity key={image} onPress ={changeOpacity}>
-          <Image style={{opacity: opacity, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        <TouchableOpacity key={image} onPress ={() => changeOpacity1()}>
+          <Image style={{opacity: opacity1, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
         </TouchableOpacity>
       )
     }
+    
     else if(count1 == 1) {
-      count1 = count1 + 1;
+      count1 = 2;
       return (
-        <TouchableOpacity key={image} onPress ={changeOpacity}>
-          <Image style={{opacity: opacity, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        <TouchableOpacity key={image} onPress ={() => changeOpacity2()}>
+          <Image style={{opacity: opacity2, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
         </TouchableOpacity>
       )
     }
     else if (count1 == 2) {
-      count1 = 0;
       return (
-        <TouchableOpacity key={image} onPress ={changeOpacity}>
-          <Image style={{opacity: opacity, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        <TouchableOpacity key={image} onPress ={() => changeOpacity3()}>
+          <Image style={{opacity: opacity3, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
         </TouchableOpacity>
       )
     }
    });
    var images2 = pics2.map(function(image) {
-    return (
-      <TouchableOpacity key={image} onPress ={changeIndex}>
-        <Image style={{height: 125, width: 125, borderColor: 'black', borderWidth: 1}} onPress ={changeIndex} source={{ uri: image}} ></Image>
-      </TouchableOpacity>
-    )
+    if(count2 == 0) {
+      count2 = 1;
+      return (
+        <TouchableOpacity key={image} onPress ={() => changeOpacity4()}>
+          <Image style={{opacity: opacity4, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        </TouchableOpacity>
+      )
+    }
+    
+    else if(count2 == 1) {
+      count2 = 2;
+      return (
+        <TouchableOpacity key={image} onPress ={() => changeOpacity5()}>
+          <Image style={{opacity: opacity5, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        </TouchableOpacity>
+      )
+    }
+    else if (count2 == 2) {
+      return (
+        <TouchableOpacity key={image} onPress ={() => changeOpacity6()}>
+          <Image style={{opacity: opacity6, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        </TouchableOpacity>
+      )
+    }
    });
    var images3 = pics3.map(function(image) {
-    return (
-      <TouchableOpacity key={image} onPress ={changeIndex}>
-        <Image style={{height: 125, width: 125, borderColor: 'black', borderWidth: 1}} onPress ={changeIndex} source={{ uri: image}} ></Image>
-      </TouchableOpacity>
-    )
+    if(count3 == 0) {
+      count3 = 1;
+      return (
+        <TouchableOpacity key={image} onPress ={() => changeOpacity7()}>
+          <Image style={{opacity: opacity7, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        </TouchableOpacity>
+      )
+    }
+    
+    else if(count3 == 1) {
+      count3 = 2;
+      return (
+        <TouchableOpacity key={image} onPress ={() => changeOpacity8()}>
+          <Image style={{opacity: opacity8, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        </TouchableOpacity>
+      )
+    }
+    else if (count3 == 2) {
+      return (
+        <TouchableOpacity key={image} onPress ={() => changeOpacity9()}>
+          <Image style={{opacity: opacity9, height: 125, width: 125, borderColor: 'black', borderWidth: 1}} source={{ uri: image}} ></Image>
+        </TouchableOpacity>
+      )
+    }
     });
 
   return (
@@ -150,7 +293,7 @@ const Profile = ({ navigation }) => {
               </View>
 
               <View style ={{alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{fontSize: 16, color: '#560CCE', margin: 27}}>Select photos</Text>
+              <Text style={{fontSize: 16, color: '#560CCE', margin: 27}}>{counter} Photos Selected</Text>
               </View>
 
               <View style ={{alignItems: 'center', justifyContent: 'center'}}>
