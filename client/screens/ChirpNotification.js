d28c4106f70164ebe9bc7168524bd137ab2aa5b9
jmultani1 <jmultani@ucsd.edu>
2022-08-19 16:42:19 -0700

    moving

--- client/screens/ChirpNotification.js
+++ client/screens/ChirpNotification.js
@@ -17,7 +17,7 @@ import MainHeader from "../components/MainHeader.js";
 import Bird_Drawing from "../assets/svg/Bird_Drawing";
 //import Constants from "../constants/constants.js";
 import Constants from 'expo-constants';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
 
@@ -30,12 +30,14 @@ const ChirpNotification = ({ navigation }) => {
     }),
   });
 
-  const user = useSelector(state => state.data.userInfo);
+  
   const [expoPushToken, setExpoPushToken] = useState('');
   const [notification, setNotification] = useState(false);
   const [test, setTest] = useState(false);
   const notificationListener = useRef();
   const responseListener = useRef();
+  const user = useSelector(state => state.data.userInfo);
+  const dispatch = useDispatch();
   let names = ['Jack Multani', 'Michael Jordan', 'Lebron James', 'Steph Curry'];
   let pics = ['file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/3A9B8F43-006A-47CF-AE34-990959BC590D.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/0A6C8202-8AE8-4A7B-8460-6A113F1CA43E.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/EEFD7E73-1A06-4B90-9280-DAAB6B26521F.jpg', 'file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/6B96B2F8-6044-4C3C-A2A5-38201C32480D.jpg']
 
@@ -45,6 +47,7 @@ const ChirpNotification = ({ navigation }) => {
     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
       names.push("Bird");
       pics.push("file:///var/mobile/Containers/Data/Application/6525879C-DFA1-4D73-BC39-9EA131D452A5/Library/Caches/ExponentExperienceData/%2540quocduyvu6262%252FBirdNest/ImagePicker/7B3C18C3-9ED8-4DBE-B65A-9D7BE3FD3D93.jpg");
+      
       setNotification(notification);
     });
 
