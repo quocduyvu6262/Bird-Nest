94dfbc04c41927b543d7a5d54b54536d1d52fba0
Pinoyburn <deondre.garcia@yahoo.com>
2022-08-23 03:03:26 -0700

    finish edit profile

--- client/screens/EditProfile.js
+++ client/screens/EditProfile.js
@@ -8,10 +8,15 @@ import {
 import React, { useState } from "react";
 import MainHeader from "../components/MainHeader";
 import { Slider } from "@rneui/themed";
+import DropDownPicker from "react-native-dropdown-picker";
+import Axios from "axios";
+import Constants from "../constants/constants";
+import * as SecureStore from "expo-secure-store";
 
 // import Redux functionality
 import { useDispatch, useSelector } from "react-redux";
 import * as dataActions from "../redux/slices/data";
+import { TextInput } from "react-native-paper";
 
 const EditProfile = ({ navigation }) => {
   /**
@@ -23,19 +28,41 @@ const EditProfile = ({ navigation }) => {
 
   // state declarations
   const [sliderValue, setSliderValue] = useState(housing.rent);
-  const [leaseTerm, setLeaseTerm] = useState(null);
+  const [leaseTerm, setLeaseTerm] = useState(housing.lease);
+  const [bioText, setBioText] = useState(userInfo.bio);
+  const [isNeighborhoodOpen, setIsNeighborhoodOpen] = useState(false); // neighborhood dropdown list
+  const [neighborhoodValue, setNeighborhoodValue] = useState(null);
+  const [neighborhoodItems, setNeighborhoodItems] = useState([
+    { label: "Downtown SD", value: "Downtown SD" },
+    { label: "La Jolla", value: "La Jolla" },
+    { label: "Del Mar", value: "Del Mar" },
+    { label: "Mira Mesa", value: "Mira Mesa" },
+    { label: "Pacific Beach", value: "Pacific Beach" },
+    { label: "Clairemont", value: "Clairemont" },
+    { label: "University City", value: "University City" },
+    { label: "UTC", value: "UTC" },
+    { label: "Solana Beach", value: "Solana Beach" },
+    { label: "Mission Valley", value: "Mission Valley" },
+    { label: "Carmel Valley", value: "Carmel Valley" },
+    { label: "Sorrento Valley", value: "Sorrento Valley" },
+    { label: "Other", value: "Other" },
+  ]);
+
+  console.log(userInfo.id);
+  console.log(userInfo.role);
+
+  // might need this as a fix later for the dropdown; ignore for now
+  //   DropDownPicker.setListMode("SCROLLVIEW");
 
   /**
    * Pull data from Redux Store and store into
    * Database and Secure Storage
    */
   const storeData = async () => {
-    const user = this.props.data.userInfo;
-    const housing = this.props.data.housing;
     // Store into Secure Store
     SecureStore.setItemAsync(
       Constants.MY_SECURE_AUTH_STATE_KEY_USER,
-      JSON.stringify(user)
+      JSON.stringify(userInfo)
     );
     SecureStore.setItemAsync(
       Constants.MY_SECURE_AUTH_STATE_KEY_HOUSING,
@@ -45,16 +72,16 @@ const EditProfile = ({ navigation }) => {
     // Store user into database
     // TODO: Implement the method to store user data into database
     Axios.post(`${await Constants.BASE_URL()}/api/users/questionnaire`, {
-      userInfo: user,
+      userInfo: userInfo,
     }).catch((err) => {
       console.log("Fail to store user into database from questionnaire");
     });
     // Store housing into database
     // TODO: Implement the method to store housing data into database
-    if (user.role === "Flamingo" || user.role === "Owl") {
+    if (userInfo.role === "Flamingo" || userInfo.role === "Owl") {
       // Post to housing
       Axios.post(`${await Constants.BASE_URL()}/api/housings/create`, {
-        user_id: user.id,
+        user_id: userInfo.id,
         housing: housing,
       })
         .then()
@@ -63,13 +90,13 @@ const EditProfile = ({ navigation }) => {
           console.log("Fail to update/insert housing from questionnaire");
         });
     } else if (
-      user.role === "Parrot" ||
-      user.role === "Penguin" ||
-      user.role === "Duck"
+      userInfo.role === "Parrot" ||
+      userInfo.role === "Penguin" ||
+      userInfo.role === "Duck"
     ) {
       // Post to nohousing
       Axios.post(`${await Constants.BASE_URL()}/api/nohousing/create`, {
-        user_id: user.id,
+        user_id: userInfo.id,
         housing: housing,
       })
         .then()
@@ -92,32 +119,49 @@ const EditProfile = ({ navigation }) => {
     dispatch(dataActions.updateLease(value));
   };
 
+  const handleBioText = (text) => {
+    setBioText(text);
+    dispatch(dataActions.updateBio(text));
+  };
+
+  const handleSave = async () => {
+    await storeData();
+    navigation.goBack();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <MainHeader screen="Edit Profile" navigation={navigation} />
-      <Text>Rent: ${sliderValue}</Text>
-      <Slider
-        value={sliderValue}
-        maximumValue={5000}
-        minimumValue={0}
-        step={25}
-        onValueChange={(value) => handleSliderChange(value)}
-        style={styles.slider}
-        thumbStyle={{ height: 15, width: 15, backgroundColor: "#6736B6" }}
-      />
+      <Text style={[styles.headerText, { marginTop: 20 }]}>Change Rent</Text>
+      <View style={styles.sliderWrapper}>
+        <View style={styles.sliderText}>
+          <Text>${sliderValue}</Text>
+        </View>
+        <Slider
+          value={sliderValue}
+          maximumValue={5000}
+          minimumValue={0}
+          step={25}
+          onValueChange={(value) => handleSliderChange(value)}
+          style={styles.slider}
+          thumbStyle={{ height: 15, width: 15, backgroundColor: "#6736B6" }}
+        />
+      </View>
 
-      <Text>Choose Lease Term (months)</Text>
+      <View style={styles.separator} />
+
+      <Text style={styles.headerText}>Choose Lease Term (months)</Text>
       {/* Radio Button Logic */}
       <View style={styles.radioButtonQuestion}>
         <View style={styles.radioButtonGroup}>
           <View style={styles.radioButtonAnswerCombo}>
             <TouchableOpacity
-              onPress={() => handleLeaseTerm("1 - 3")}
+              onPress={() => handleLeaseTerm("1 to 3")}
               style={[
                 styles.radioButton,
                 {
                   backgroundColor:
-                    leaseTerm === "1 - 3" ? "#560CCE" : "lightgray",
+                    leaseTerm === "1 to 3" ? "#560CCE" : "lightgray",
                 },
               ]}
             />
@@ -125,12 +169,12 @@ const EditProfile = ({ navigation }) => {
           </View>
           <View style={styles.radioButtonAnswerCombo}>
             <TouchableOpacity
-              onPress={() => handleLeaseTerm("4 - 7")}
+              onPress={() => handleLeaseTerm("4 to 7")}
               style={[
                 styles.radioButton,
                 {
                   backgroundColor:
-                    leaseTerm === "4 - 7" ? "#560CCE" : "lightgray",
+                    leaseTerm === "4 to 7" ? "#560CCE" : "lightgray",
                 },
               ]}
             />
@@ -141,12 +185,12 @@ const EditProfile = ({ navigation }) => {
         <View style={styles.radioButtonGroup}>
           <View style={styles.radioButtonAnswerCombo}>
             <TouchableOpacity
-              onPress={() => handleLeaseTerm("8 - 11")}
+              onPress={() => handleLeaseTerm("8 to 11")}
               style={[
                 styles.radioButton,
                 {
                   backgroundColor:
-                    leaseTerm === "8 - 11" ? "#560CCE" : "lightgray",
+                    leaseTerm === "8 to 11" ? "#560CCE" : "lightgray",
                 },
               ]}
             />
@@ -167,6 +211,44 @@ const EditProfile = ({ navigation }) => {
           </View>
         </View>
       </View>
+
+      <View style={styles.separator} />
+
+      {/* Neighborhood Dropdown List */}
+      <Text style={styles.headerText}>Select Neighborhood</Text>
+      <View style={styles.dropdown}>
+        <DropDownPicker
+          open={isNeighborhoodOpen}
+          value={neighborhoodValue}
+          items={neighborhoodItems}
+          setOpen={setIsNeighborhoodOpen}
+          setValue={setNeighborhoodValue}
+          setItems={setNeighborhoodItems}
+          onSelectItem={(item) =>
+            dispatch(dataActions.updateNeighborhood(item.value))
+          }
+          style={{ backgroundColor: "#560CCE" }}
+          textStyle={{ fontSize: 18, color: "white", fontWeight: "bold" }}
+          dropDownContainerStyle={{ backgroundColor: "#560CCE" }}
+        />
+      </View>
+
+      <View style={styles.separator} />
+
+      {/* change bio input */}
+      <Text style={styles.headerText}>Change Your Bio</Text>
+      <TextInput
+        label="Biography"
+        placeholder="Enter Bio"
+        value={bioText}
+        onChangeText={(text) => handleBioText(text)}
+        mode="outlined"
+        style={{ width: 300, height: 50, alignSelf: "center" }}
+      />
+
+      <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
+        <Text style={styles.saveButtonText}>Save Changes</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -177,9 +259,39 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  headerText: {
+    alignSelf: "center",
+    fontSize: 25,
+    // marginTop: 30,
+    marginBottom: 10,
+  },
+  separator: {
+    height: 1,
+    width: "90%",
+    backgroundColor: "#560CCE",
+    alignSelf: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  sliderWrapper: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
   slider: {
     width: 260,
-    left: 50,
+  },
+  sliderText: {
+    // backgroundColor: "red",
+    // alignContent: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 25,
+    width: 55,
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: "#560CCE",
+    marginRight: 20,
   },
   radioButtonAnswerCombo: {
     flexDirection: "row",
@@ -200,6 +312,27 @@ const styles = StyleSheet.create({
   },
   radioButtonText: {
     marginLeft: 10,
+  },
+  dropdown: {
+    width: 300,
+    justifyContent: "center",
+    alignSelf: "center",
+    zIndex: 10,
+  },
+  saveButton: {
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: 200,
+    backgroundColor: "#560CCE",
+    marginTop: 80,
+    borderRadius: 5,
+  },
+  saveButtonText: {
+    color: "white",
+    fontSize: 23,
+    fontWeight: "bold",
   },
 });
 
