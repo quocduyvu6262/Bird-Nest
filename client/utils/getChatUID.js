b95190308b389081961ad06dd69ae03154940a68
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-08-23 20:12:20 -0400

    remove bubble

--- client/utils/getChatUID.js
+++ client/utils/getChatUID.js
@@ -10,3 +10,18 @@ export const getChatUID = (fullname, uid) => {
     const chatId = `${trimName}_${uid}`;
     return chatId
 }
+
+/**
+ * Remove item from given array
+ * @param array the array contain the item to be removed
+ * @param item the item to be removed
+ * @returns the new array
+ */
+export const removeItem = (array, item) => {
+    const newArray = Array.from(array);
+    const index = newArray.indexOf(item);
+    if (index > -1) { // only splice array when item is found
+        newArray.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    return newArray;
+}
