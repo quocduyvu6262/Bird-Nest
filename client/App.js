9c965f2cc0864352c01e16989dac2c5a6401478d
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-08-24 19:51:40 -0400

    merged

--- client/App.js
+++ client/App.js
@@ -30,7 +30,7 @@ import HasHousingQ from "./screens/Questionnaires/HasHousingQ.js";
 import Personality from "./screens/Questionnaires/Personality.js";
 import BasicInfo from "./screens/Questionnaires/BasicInfo.js";
 import EditProfile from "./screens/EditProfile";
-
+import UserProfile from './components/UserProfile';
 // IMPORT CHAT NAVIGATOR
 import ChatNavigator from "./screens/ChatAPI/ChatNavigator";
 
@@ -131,6 +131,7 @@ export default function App() {
             <Stack.Screen name="Personality" component={Personality} />
             <Stack.Screen name="Roles" component={Roles} />
             <Stack.Screen name="History" component={History} />
+            <Stack.Screen name="UserProfile" component={UserProfile} />
             <Stack.Screen name="EditProfile" component={EditProfile} />
           </Stack.Navigator>
         </NavigationContainer>
