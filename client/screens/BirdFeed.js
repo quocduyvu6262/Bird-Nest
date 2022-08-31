a43d6fe616f3e37cbdce6d0669512131e0738b31
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-08-31 13:26:03 -0400

    merged jack

--- client/screens/BirdFeed.js
+++ client/screens/BirdFeed.js
@@ -11,10 +11,7 @@ import {
   Platform,
   StatusBar,
   ScrollView,
-<<<<<<< HEAD
   Switch,
-=======
->>>>>>> dev
 } from "react-native";
 import Svg, { Path } from "react-native-svg";
 import Bird_Drawing from "../assets/svg/Bird_Drawing.js";
@@ -37,9 +34,6 @@ import { useFonts, Pacifico_400Regular } from "@expo-google-fonts/pacifico";
 import MainHeader from "../components/MainHeader.js";
 import Constants from "../constants/constants.js";
 import barackObama from "../assets/barackObama.jpeg";
-<<<<<<< HEAD
-
-=======
 import { useChatClient } from "./ChatAPI/useChatClient.js";
 import FilterOverlay from "../components/FilterOverlay.js";
 // Old Imports for filter
@@ -47,7 +41,6 @@ import FilterOverlay from "../components/FilterOverlay.js";
 // import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
 import Icon3 from "react-native-vector-icons/Ionicons";
 import { useSelector } from "react-redux";
->>>>>>> dev
 const BirdFeed = ({ navigation }) => {
   const user = useSelector((state) => state.data.userInfo);
   const [open, setOpen] = useState(false);
@@ -71,52 +64,7 @@ const BirdFeed = ({ navigation }) => {
 
   const [userList, setUserList] = useState([]);
   const [listState, setListState] = useState(false);
-<<<<<<< HEAD
-  const [overlayClicked, setOverlayClicked] = useState(false);
-  const [backgroundGrey, setBackgroundGrey] = useState(false);
-
-  const [switchEnabledNeigh, setSwitchEnabledNeigh] = useState(false);
-  const toggleSwitchNeigh = () =>
-    setSwitchEnabledNeigh((previousState) => !previousState);
-
-  const [switchEnabledSqua, setSwitchEnabledSqua] = useState(false);
-  const toggleSwitchSqua = () =>
-    setSwitchEnabledSqua((previousState) => !previousState);
-
-  const [switchEnabledPri, setSwitchEnabledPri] = useState(false);
-  const toggleSwitchPri = () =>
-    setSwitchEnabledPri((previousState) => !previousState);
-
-  const [switchEnabledIn, setSwitchEnabledIn] = useState(false);
-  const toggleSwitchIn = () =>
-    setSwitchEnabledIn((previousState) => !previousState);
-
-  const [switchEnabledPer, setSwitchEnabledPer] = useState(false);
-  const toggleSwitchPer = () =>
-    setSwitchEnabledPer((previousState) => !previousState);
-
-  const [switchEnabledRoo, setSwitchEnabledRoo] = useState(false);
-  const toggleSwitchRoo = () =>
-    setSwitchEnabledRoo((previousState) => !previousState);
-
-  const [switchEnabledYes, setSwitchEnabledYes] = useState(false);
-  const toggleSwitchYes = () =>
-    setSwitchEnabledYes((previousState) => !previousState);
-
-  const [switchEnabledNo, setSwitchEnabledNo] = useState(false);
-  const toggleSwitchNo = () =>
-    setSwitchEnabledNo((previousState) => !previousState);
-
-  const [switchEnabledRec, setSwitchEnabledRec] = useState(false);
-  const toggleSwitchRec = () =>
-    setSwitchEnabledRec((previousState) => !previousState);
-
-  const [switchEnabledApt, setSwitchEnabledApt] = useState(false);
-  const toggleSwitchApt = () =>
-    setSwitchEnabledApt((previousState) => !previousState);
-=======
   // This is the old filter function on birdfeed
->>>>>>> dev
 
   const overlayFilterButton = () => {
     overlayFilterClicked
@@ -172,35 +120,10 @@ const BirdFeed = ({ navigation }) => {
   });
   // ----- LOGIC FOR VIEW USER BUTTONS -----
 
-<<<<<<< HEAD
-  const SingleSwitch = (props) => {
-    return (
-      <View style={styles.switchView}>
-        <Switch
-          trackColor={{ false: "%767577", true: "green" }}
-          thumbColor={props.enabled ? "#white" : "white"}
-          onValueChange={props.toggle}
-          value={props.enabled}
-        ></Switch>
-        <Text style={styles.switchText}>
-          <Text></Text>
-          {props.variable}
-        </Text>
-      </View>
-    );
-  };
-  // ----- LOGIC FOR VIEW USER BUTTONS -----
-
-  const viewUsers = () => {
-    setUserList([]);
-    Axios.post(`${Constants.BASE_URL}/api/matching/`, {
-      user_id: 10,
-=======
   const viewUsers = async () => {
     setUserList([]);
     Axios.post(`${await Constants.BASE_URL()}/api/matching/`, {
       user_id: user.id,
->>>>>>> dev
     })
       .then((response) => {
         let userData = response.data;
@@ -230,13 +153,10 @@ const BirdFeed = ({ navigation }) => {
     setListState(true);
   };
 
-<<<<<<< HEAD
   const overlayButton = () => {
     overlayClicked ? setOverlayClicked(false) : setOverlayClicked(true);
   };
 
-=======
->>>>>>> dev
   useEffect(() => {
     viewUsers();
   }, []);
@@ -250,23 +170,9 @@ const BirdFeed = ({ navigation }) => {
       // Header - Beginning
       <SafeAreaView style={styles.container}>
         <MainHeader screen="Bird Feed" navigation={navigation} />
-<<<<<<< HEAD
         <TouchableOpacity
           style={[styles.input, { marginVertical: 7 }]}
           onPress={overlayButton}
-=======
-        <View
-          style={[
-            styles.svg,
-            { transform: [{ translateY: 20 }, { translateX: 100 }] },
-          ]}
-        >
-          <Bird_Drawing />
-        </View>
-        <TouchableOpacity
-          style={[styles.input, { marginVertical: 7 }]}
-          onPress={overlayFilterButton}
->>>>>>> dev
         >
           <Icon3
             style={styles.input}
@@ -275,7 +181,6 @@ const BirdFeed = ({ navigation }) => {
             color="black"
           />
         </TouchableOpacity>
-<<<<<<< HEAD
 
         {overlayClicked && (
           <View style={styles.subContainer}>
@@ -348,49 +253,6 @@ const BirdFeed = ({ navigation }) => {
               />
             </ScrollView>
           </View>
-=======
-        {overlayFilterClicked && (
-          <FilterOverlay
-            setOverlayFilterClicked={setOverlayFilterClicked}
-            overlaFilterClicked={overlayFilterClicked}
-            overlayFilterButton={overlayFilterButton}
-            open={open}
-            setOpen={setOpen}
-            value={value}
-            setValue={setValue}
-            items={items}
-            setItems={setItems}
-            itemcount={itemcount}
-            setAgeState={setAgeState}
-            ageState={ageState}
-            setNeighborhood={setNeighborhood}
-            neighborhood={neighborhood}
-            setRentState={setRentState}
-            rentState={rentState}
-            setLeaseState={setLeaseState}
-            leaseState={leaseState}
-            setSqFtState={setSqFtState}
-            sqFtState={sqFtState}
-            switchEnabledPar={switchEnabledPar}
-            setSwitchEnabledPar={setSwitchEnabledPar}
-            toggleSwitchPar={toggleSwitchPar}
-            switchEnabledGym={switchEnabledGym}
-            setSwitchEnabledGym={setSwitchEnabledGym}
-            toggleSwitchGym={toggleSwitchGym}
-            switchEnabledPoo={switchEnabledPoo}
-            setSwitchEnabledPoo={setSwitchEnabledPoo}
-            toggleSwitchPoo={toggleSwitchPoo}
-            switchEnabledApp={switchEnabledApp}
-            setSwitchEnabledApp={setSwitchEnabledApp}
-            toggleSwitchApp={toggleSwitchApp}
-            switchEnabledFur={switchEnabledFur}
-            setSwitchEnabledFur={setSwitchEnabledFur}
-            toggleSwitchFur={toggleSwitchFur}
-            switchEnabledAC={switchEnabledAC}
-            setSwitchEnabledAC={setSwitchEnabledAC}
-            toggleSwitchAC={toggleSwitchAC}
-          />
->>>>>>> dev
         )}
 
         {listState && (
@@ -492,15 +354,12 @@ const styles = StyleSheet.create({
     width: "100%",
     position: "absolute",
     zIndex: 1,
-<<<<<<< HEAD
-=======
   },
   svg: {
     position: "absolute",
     zIndex: 5,
     // top: 100,
     // left: 200,
->>>>>>> dev
   },
 });
 export default BirdFeed;
